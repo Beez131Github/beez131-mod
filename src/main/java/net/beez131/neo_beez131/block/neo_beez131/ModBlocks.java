fd4abc92869ba0ab90a612d840a13410c8e577cb
Beez131Github <olivercollins123@hotmail.com>
2025-01-14 13:04:25 +1300

    new block fix (thanks to Tarantel)

--- src/main/java/net/beez131/neo_beez131/block/neo_beez131/ModBlocks.java
+++ src/main/java/net/beez131/neo_beez131/block/neo_beez131/ModBlocks.java
@@ -20,7 +20,7 @@ public class ModBlocks {
     public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(Neo_Beez131.MOD_ID);
 
     public static final DeferredBlock<Block> ENDERITE_BLOCK = registerBlock("enderite_block", () -> new Block(BlockBehaviour.Properties.of()
-            .strength(4f).requiresCorrectToolForDrops().sound(SoundType.NETHERITE_BLOCK)));
+            .strength(4f).requiresCorrectToolForDrops().sound(SoundType.NETHERITE_BLOCK).setId(ResourceKey.create(Registries.BLOCK, rl("enderite_block")))));
 
     private static <T extends Block> DeferredBlock<T> registerBlock(String name, Supplier<T> block) {
         DeferredBlock<T> toReturn = BLOCKS.register(name, block);
@@ -30,6 +30,11 @@ public class ModBlocks {
 
     private static <T extends Block> void registerBlockItem(String name, DeferredBlock<T> block) {
         ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties().setId(ResourceKey.create(Registries.ITEM, ResourceLocation.fromNamespaceAndPath(Neo_Beez131.MOD_ID, name)))));
+    }
+
+    public static ResourceLocation rl(String name)
+    {
+        return ResourceLocation.fromNamespaceAndPath(Neo_Beez131.MOD_ID, name);
     }
 
     public static void register(IEventBus eventBus) {
